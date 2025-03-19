7fa3b42070f160b52c14db4cf1ffb9476f90fe9e
Wiillamss <willamsgomes95@gmail.com>
2025-03-19 13:39:34 +0000

    commint

--- xadrez.c
+++ xadrez.c
@@ -3,6 +3,7 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+// Prática Willams da Silva Gomes
 
 #include <stdio.h>
 
